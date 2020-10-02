7b960305944f0717939a94e6c0f5dd27fd26353c
Andrew <andrewgraemebrooks@protonmail.com>
2020-10-02 14:46:59 +0100

    Create event page now works

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+// use App\Form\EventType;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use \App\Entity\Event;
-use Psr\Log\LoggerInterface;
 
 class EventController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="index")
      */
     public function index()
     {
@@ -27,17 +32,53 @@ class EventController extends AbstractController
     }
 
     /**
-     * @Route("/event/create")
+     * @Route("/show-events", name="show-events")
      */
-    public function create()
+    public function show()
     {
+        $events = $this->getDoctrine()->getRepository(Event::class)->findAll();
 
+        return $this->render('event/show.html.twig', array('events' => $events));
     }
 
     /**
-     * @Route("/event/save")
+     * @Route("/create-event")
      */
-    public function save(LoggerInterface $logger)
+    function new (Request $request) {
+        $event = new Event();
+        // $form = $this->createForm(EventType::class, $event, array('attr' => array('class' => 'form-control')));
+        $form = $this->createFormBuilder($event)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control mb-3')))
+            ->add('description', TextType::class, array('attr' => array('class' => 'form-control mb-3')))
+            ->add('date', DateTimeType::class, array('attr' => array('class' => 'mb-3')))
+            ->add('duration', NumberType::class, array('attr' => array('class' => 'form-control mb-3')))
+            ->add('venue', TextType::class, array('attr' => array('class' => 'form-control mb-3')))
+            ->add('save', SubmitType::class, array(
+                'attr' => array('class' => 'btn btn-success'),
+                'label' => 'Create',
+            ))->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("index");
+        }
+
+        return $this->render('event/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("api/event/save")
+     */
+    public function save()
     {
         // Get the entity manager.
         $entityManager = $this->getDoctrine()->getManager();
@@ -49,13 +90,6 @@ class EventController extends AbstractController
         $event->setDate(new DateTime());
         $event->setDuration(120);
         $event->setVenue("Venue");
-
-        // $log->pushHandler(new StreamHandler(__DIR__ . '/my_app.log', Logger::DEBUG));
-        // $log->pushHandler(new FirePHPHandler());
-
-        // You can now use your logger
-        $logger->info('My logger is now ready');
-        $logger->debug('Event Object', ['event' => $event]);
 
         // Persist tells the entity manager that we want to save the $event object.
         $entityManager->persist($event);
