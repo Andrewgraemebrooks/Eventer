0aad910cd8497bc329ea2d7c1090200edfe08f34
Andrew <andrewgraemebrooks@protonmail.com>
2020-11-17 13:16:38 +0000

    Added delete event route.
    Problem(s):
    * There isn't a route to delete an event in the database.
    
    Solution(s):
    * Added route to delete an event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -89,11 +90,13 @@ class EventController extends Controller
      * Remove the specified resource from storage.
      *
      * @param \App\Models\Event $event
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     * @throws Exception
      */
     public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return redirect('/events');
     }
 
     /**
---
+++ tests/Feature/Events/DeleteEventTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DeleteEventTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * Events can be deleted
+     *
+     * @return void
+     */
+    public function testEventCanBeDeleted()
+    {
+        // Remove exception handling to ensure that Laravel doesn't hide exception details.
+        $this->withoutExceptionHandling();
+
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create an Event
+        $this->post('/event', [
+            'name' => 'Event',
+            'description' => 'Event Description',
+            'date' => now(),
+            'time' => '12:00:00',
+            "duration" => '120',
+            "venue" => 'Event Venue',
+        ]);
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get event id
+        $eventId = Event::all()->first()->id;
+
+        // Delete event
+        $response = $this->delete('/event/' . $eventId);
+
+        // Assert the event has been deleted
+        $this->assertCount(0, Event::all());
+
+        // Assert that the user has been redirected
+        $response->assertRedirect('/events');
+    }
+
+    /**
+     * Allows the test to authenticate routes as a user
+     * @returns void
+     * @author Andrew Brooks
+     */
+    protected function actAsUser(): void
+    {
+        // Create a user.
+        User::factory()->create();
+
+        // Assert that the user has been added to the database.
+        $this->assertCount(1, User::all());
+
+        // Act as the newly created user.
+        $this->actingAs(User::all()->first());
+    }
+}
