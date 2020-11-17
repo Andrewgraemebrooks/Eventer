7c52ad42350c6beb8a236c8caa5e3d3f2c427349
Andrew <andrewgraemebrooks@protonmail.com>
2020-11-17 20:51:00 +0000

    Added update event validation unit tests
    Problem(s):
    * The update events route lacks unit testing to ensure that the validation works.
    
    Solution(s):
    * Add unit tests to ensure that the update event validation works

--- tests/Feature/Events/UpdateEventTest.php
+++ tests/Feature/Events/UpdateEventTest.php
@@ -24,15 +24,7 @@ class UpdateEventTest extends TestCase
         // Act as a user to authenticate route.
         $this->actAsUser();
 
-        // Create an Event
-        $this->post('/event', [
-            'name' => 'Event',
-            'description' => 'Event Description',
-            'date' => now(),
-            'time' => '12:00:00',
-            "duration" => '120',
-            "venue" => 'Event Venue',
-        ]);
+        $this->createEvent('Event', 'Event Description', now(), '12:00:00', '120', 'Event Venue');
 
         // Assert that the event is stored in the database.
         $this->assertCount(1, Event::all());
@@ -40,14 +32,15 @@ class UpdateEventTest extends TestCase
         // Get event id
         $eventId = Event::all()->first()->id;
 
-        $response = $this->patch('/event/' . $eventId, [
-            'name' => 'Updated Event',
-            'description' => 'Updated Event Description',
-            'date' => now(),
-            'time' => '12:00:00',
-            "duration" => '120',
-            "venue" => 'Updated Event Venue',
-        ]);
+        $response = $this->updateEvent(
+            $eventId,
+            'Updated Event',
+            'Updated Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Updated Event Venue'
+        );
 
         // Assert that the event has been updated.
         $this->assertEquals('Updated Event', Event::all()->first()->name);
@@ -56,6 +49,604 @@ class UpdateEventTest extends TestCase
 
         // Assert that the route redirects the user back to the event page.
         $response->assertRedirect('/event/' . $eventId);
+    }
+
+    /**
+     * An event must have a name.
+     * @return void
+     */
+    public function testUpdateNameIsRequired()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        $eventId = Event::all()->first()->id;
+
+        $response = $this->updateEvent(
+            $eventId,
+            '',
+            'Updated Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Updated Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('name');
+    }
+
+    /**
+     * An event must have a description.
+     * @return void
+     */
+    public function testUpdateDescriptionIsRequired()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            '',
+            now(),
+            '12:00:00',
+            '120',
+            'Updated Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('description');
+    }
+
+    /**
+     * An event must have a date.
+     * @return void
+     */
+    public function testUpdateDateIsRequired()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            "",
+            '12:00:00',
+            '120',
+            'Updated Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('date');
+    }
+
+    /**
+     * An event must have a time.
+     * @return void
+     */
+    public function testUpdateTimeIsRequired()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '',
+            '120',
+            'Updated Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('time');
+    }
+
+    /**
+     * An event must have a duration.
+     * @return void
+     */
+    public function testUpdateDurationIsRequired()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '',
+            'Updated Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('duration');
+    }
+
+    /**
+     * An event must have a venue.
+     * @return void
+     */
+    public function testUpdateVenueIsRequired()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            ''
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('venue');
+    }
+
+    /**
+     * An event name must be at least three characters.
+     * @return void
+     */
+    public function testUpdateNameNeedsThreeMinCharacters()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Ev',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('name');
+    }
+
+    /**
+     * An event name must be equal to or less than sixty characters.
+     * @return void
+     */
+    public function testUpdateNameNeedsSixtyMaxCharacters()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'yNO21DhVnIOQCnb7kJuGhraUA2FzvmFTzK44tbestjNMBG1z7lRSJKD2CUXxm',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('name');
+    }
+
+    /**
+     * An event description must be equal to or less than sixty characters.
+     * @return void
+     */
+    public function testUpdateDescriptionNeedsSixtyMaxCharacters()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'yNO21DhVnIOQCnb7kJuGhraUA2FzvmFTzK44tbestjNMBG1z7lRSJKD2CUXxm',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('description');
+    }
+
+    /**
+     * An event date must be in the proper date format.
+     * @return void
+     */
+    public function testUpdateDateMustBeTheRightFormat()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            '12:00:00',
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('date');
+    }
+
+    /**
+     * An event time must be in the proper time format.
+     * @return void
+     */
+    public function testUpdateTimeMustBeTheRightFormat()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('time');
+    }
+
+    /**
+     * An event duration must be more than zero
+     * @return void
+     */
+    public function testUpdateDurationIsMoreThanZero()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '0',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('duration');
+    }
+
+    /**
+     * An event duration must be less than 1,440 minutes (24 hours)
+     * @return void
+     */
+    public function testUpdateDurationIsLessThanADay()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '1441',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('duration');
+    }
+
+    /**
+     * An event venue must be equal to or less than sixty characters.
+     * @return void
+     */
+    public function testUpdateVenueNeedsSixtyMaxCharacters()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'yNO21DhVnIOQCnb7kJuGhraUA2FzvmFTzK44tbestjNMBG1z7lRSJKD2CUXxm'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('venue');
+    }
+
+    /**
+     * An event duration must be an integer.
+     * @return void
+     */
+    public function testUpdateDurationMustBeAnInteger()
+    {
+        // Act as a user to authenticate route.
+        $this->actAsUser();
+
+        // Create the event.
+        $this->createEvent(
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            '120',
+            'Event Venue'
+        );
+
+        // Assert that the event is stored in the database.
+        $this->assertCount(1, Event::all());
+
+        // Get the id of the event
+        $eventId = Event::all()->first()->id;
+
+        // Update the event
+        $response = $this->updateEvent(
+            $eventId,
+            'Event Name',
+            'Event Description',
+            now(),
+            '12:00:00',
+            'Event Duration',
+            'Event Venue'
+        );
+
+        // Assert that there is an error
+        $response->assertSessionHasErrors('duration');
     }
 
     /**
@@ -73,5 +664,56 @@ class UpdateEventTest extends TestCase
 
         // Act as the newly created user.
         $this->actingAs(User::all()->first());
+    }
+
+
+    /**
+     * @param $name
+     * @param $desc
+     * @param $date
+     * @param $time
+     * @param $duration
+     * @param $venue
+     * @return \Illuminate\Testing\TestResponse
+     */
+    protected function createEvent($name, $desc, $date, $time, $duration, $venue): \Illuminate\Testing\TestResponse
+    {
+        return $this->post('/event', [
+            'name' => $name,
+            'description' => $desc,
+            'date' => $date,
+            'time' => $time,
+            "duration" => $duration,
+            "venue" => $venue,
+        ]);
+    }
+
+    /**
+     * @param $name
+     * @param $desc
+     * @param $date
+     * @param $time
+     * @param $duration
+     * @param $venue
+     * @return \Illuminate\Testing\TestResponse
+     */
+    protected function updateEvent(
+        $eventId,
+        $name,
+        $desc,
+        $date,
+        $time,
+        $duration,
+        $venue
+    ): \Illuminate\Testing\TestResponse
+    {
+        return $this->patch('/event/' . $eventId, [
+            'name' => $name,
+            'description' => $desc,
+            'date' => $date,
+            'time' => $time,
+            "duration" => $duration,
+            "venue" => $venue,
+        ]);
     }
 }
