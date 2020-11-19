fe656fc188a7ef1d1c33195ed2048662171f53d8
Andrew <andrewgraemebrooks@protonmail.com>
2020-11-19 16:30:02 +0000

    Created a database seeder
    Problem(s):
    * We do not have a way of quickly creating dummy data and inserting the data into or database.
    
    Solution(s):
    * Created a database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
+use App\Models\Speaker;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Event::factory()->create();
+        $speakers = Speaker::factory(10)->create();
+
+        Event::All()->each(function ($event) use ($speakers) {
+            $event->speakers()->saveMany($speakers);
+        });
+
     }
 }
