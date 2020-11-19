32020e0da91cd34a244212b532c4555f8c6b5ac9
Andrew <andrewgraemebrooks@protonmail.com>
2020-11-19 16:25:54 +0000

    Created speaker model, controller, factory, and migration
    Problem(s):
    * We do not have a table to store the event speakers in.
    
    Solution(s):
    * Created a speaker model, controller, factory, and migration

---
+++ app/Models/Speaker.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Speaker extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    /**
+     * The events that belong to the speaker.
+     */
+    public function speakers()
+    {
+        return $this->belongsToMany('App\Models\Event');
+    }
+}
+
+
